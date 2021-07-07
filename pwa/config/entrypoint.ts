854848eb39a732889fc80a7549fa57d6e3fe74e8
Gael <zzgael@gmail.com>
2021-07-07 06:34:16 +0000

    ..

--- pwa/config/entrypoint.ts
+++ pwa/config/entrypoint.ts
@@ -1,1 +1,1 @@
-export const ENTRYPOINT = typeof window === "undefined" ? (process.env.NEXT_PUBLIC_ENTRYPOINT || "http://localhost") : window.origin;
+export const ENTRYPOINT = typeof window === "undefined" ? (process.env.NEXT_PUBLIC_ENTRYPOINT || "http://localhost:3000") : window.origin;
