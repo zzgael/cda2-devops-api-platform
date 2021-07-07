9230083ec9cd0c4bbcdacbf88df1a57e0bc72957
Gael <zzgael@gmail.com>
2021-07-07 12:55:14 +0000

    ..

--- pwa/config/entrypoint.ts
+++ pwa/config/entrypoint.ts
@@ -1,1 +1,1 @@
-export const ENTRYPOINT = typeof window === "undefined" ? 'http://caddy' : window.origin;
+export const ENTRYPOINT = typeof window === "undefined" ? (process.env.NEXT_PUBLIC_ENTRYPOINT || "https://cda2-devops-gael.simplon-roanne.com") : window.origin;
